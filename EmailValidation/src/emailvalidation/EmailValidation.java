b168c83750afed116bc7914445ff2e535c149cbf
jahirsheikh <jahirsheikh14@gmail.com>
2023-09-19 16:14:39 +0600

    5th Month Final Exam avidence

---
+++ EmailValidation/src/emailvalidation/EmailValidation.java
@@ -0,0 +1,19 @@
+
+package emailvalidation;
+
+import view.Email;
+
+public class EmailValidation {
+
+    public static void main(String[] args) {
+       
+        Email email = new Email();
+        
+        email.setVisible(true);
+        
+        email.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
---
+++ FromReadAndwrite/src/fromreadandwrite/FromReadAndwrite.java
@@ -0,0 +1,22 @@
+
+package fromreadandwrite;
+
+import view.From;
+
+
+
+public class FromReadAndwrite {
+
+    
+    public static void main(String[] args) {
+        
+      From form = new From();
+      
+      form.setVisible(true);
+      
+      form.setLocationRelativeTo(null);
+        
+        
+    }
+    
+}
